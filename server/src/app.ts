45efa1af2a5d5f94e4b2e0a6d7ba5419f7629b6e
unknown <nicomugas@gmail.com>
2023-03-27 12:03:58 -0300

    Server config

---
+++ server/src/app.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import morgan from 'morgan';
+
+const app = express();
+
+import authRoutes from './routes/auth'
+
+//settings
+app.set('port', 3000)
+//midleware
+app.use(morgan('dev'))
+//routes 
+app.use(authRoutes)
+
+
+export default app;
