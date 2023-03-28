6ad7958f403f24da45d76d9ce4231693e6901878
Matias Barengo <matiasbarengo99@gmail.com>
2023-03-28 17:40:19 -0300

    logIn register actualization

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,36 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Link,
-} from "react-router-dom";
-import Login from "./components/LogIn";
+import React, { useState } from "react";
+import LogIn from "./components/LogIn";
 import Register from "./components/Register";
 
-function App() {
-  return <div></div>;
+interface User {
+  email: string;
+  password: string;
 }
+
+const App = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  const handleLogIn = (email: string, password: string) => {
+    // Aquí debería hacer una petición al servidor para autenticar al usuario
+    // y almacenar el token de autenticación en el estado de la aplicación.
+    setUser({ email, password });
+  };
+
+  const handleRegister = (
+    email: string,
+    password: string
+  ) => {
+    // Aquí debería hacer una petición al servidor para crear un nuevo usuario
+    // y almacenar el token de autenticación en el estado de la aplicación.
+    setUser({ email, password });
+  };
+
+  return (
+    <div>
+      <LogIn onLogIn={handleLogIn} />
+      <Register onRegister={handleRegister} />
+    </div>
+  );
+};
 
 export default App;
