edb320d7be61caa91ef97656e73e962d7949b561
mabicruz <cruzmaribel013@gmail.com>
2023-03-29 09:44:25 -0300

    :bug: Fixed Front-End repository

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link,
+  } from "react-router-dom";
+import Header from "./components/Header";
+import Register from "./components/Register";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import { ProtectedRoute } from "./routes/protectedRouted/ProtectedRoutes";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </div>
+  )
+}
+export default App
