37bed17d012be8dcfb188591e55e941d7c1e9ba4
David <david.cicconi94@gmail.com>
2023-04-04 13:01:13 -0300

    seeder specialist added

---
+++ server/seeders/20230404151621-specialist.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const specialistData = require('../src/constans/specialists');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.bulkInsert('specialist', specialistData);
+	},
+
+	async down(queryInterface, Sequelize) {
+		await queryInterface.bulkDelete('specialist', null, {});
+	},
+};
