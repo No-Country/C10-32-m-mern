b208552deb58bd712df2218ef65a7127a1caccfd
Matias Barengo <matiasbarengo99@gmail.com>
2023-04-04 14:18:51 -0300

    register form

--- client/src/components/Text.tsx
+++ client/src/components/Text.tsx
@@ -1,11 +1,13 @@
-const Text = ({first, second, third}:any) =>{
-    return(
-        <div className="leading-3">
-            <p className="mt-6 text-[18px] font-bold">{first}</p>
-            <p className="mt-6 text-[18px] leading-normal">{second}</p>
-            <p className="mt-6 text-[18px]">{third}</p>
-        </div>
-    )
-}
+const Text = ({ first, second, third }: any) => {
+  return (
+    <div className="leading-3">
+      <p className="mt-6 text-[18px] font-bold">{first}</p>
+      <p className="mt-6 text-[18px] leading-normal">
+        {second}
+      </p>
+      <p className="mt-6 text-[18px]">{third}</p>
+    </div>
+  );
+};
 
-export default Text
+export default Text;
--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -2,14 +2,36 @@ import React, { SyntheticEvent, useState } from "react";
 import { useNavigate } from "react-router";
 import { ButtonEyeSlash } from "../../components/ButtonEyeSlash";
 import ButtonLogIn from "../../components/ButtonLogIn";
-import IconDni from "../../icons/IconDni";
-import Lock from "../../icons/Lock";
-import MailIcon from "../../icons/MailIcon";
+import HeaderSm from "../../components/HeaderSm";
+import NavBar from "../../components/NavBar";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [secondname, setSecondname] = useState("");
   const [document, setDocument] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState("");
+  const [obrasocialId, setObrasocialId] = useState("");
+
+  const obraSocial = [
+    {
+      id: 1,
+      name: "PAMI",
+    },
+    {
+      id: 2,
+      name: "IOMA",
+    },
+    {
+      id: 3,
+      name: "OSDE",
+    },
+    {
+      id: 4,
+      name: "OBRASOCIAL3",
+    },
+  ];
 
   const navigate = useNavigate();
   const [redirection, setRedirection] = useState(false);
@@ -20,13 +42,13 @@ const Register = () => {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        name: "name",
-        secondname: "asd",
-        document: "5678",
-        email: "matiass@algo.com.ar",
-        password: "1234",
-        phone: "12345678",
-        obrasocialId: 1,
+        name,
+        secondname,
+        document,
+        email,
+        password,
+        phone,
+        obrasocialId,
       }),
     });
     setRedirection(true);
@@ -37,39 +59,65 @@ const Register = () => {
   }
 
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center">
+      <HeaderSm />
       <form
-        className="h-[100%] mt-[3rem] flex flex-col items-center justify-between"
+        className="h-[100%] w-full flex flex-col gap-[1rem] items-center justify-between"
         onSubmit={submit}
       >
-        <div className="w-full h-[50px]">
-          <label htmlFor="dni" />
-          <input
-            placeholder="Numero de Documento"
-            type="number"
-            id="document"
-            value={document}
-            onChange={(e) => setDocument(e.target.value)}
-            required
-            className="relative w-full border-[.5px] border-buttonBg rounded-md pl-7"
-          />
-          <IconDni />
-        </div>
-        <div className="w-full h-[50px]">
-          <label htmlFor="email" />
-          <input
-            placeholder="Correo Electronico"
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="relative w-full border-[.5px] border-buttonBg rounded-md pl-7"
-          />
-          <MailIcon />
-        </div>
-        <div className="w-full h-[50px]">
-          <label htmlFor="password" />
+        <input
+          className="relative w-full h-[2.5rem] border-[.5px] border-borders rounded-md pl-2"
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          placeholder="Nombre"
+        />
+        <input
+          className="relative w-full h-[2.5rem] border-[.5px] border-borders rounded-md pl-2"
+          type="text"
+          id="secondname"
+          value={secondname}
+          onChange={(e) => setSecondname(e.target.value)}
+          placeholder="Apellido"
+        />
+        <input
+          placeholder="Correo Electronico"
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+          className="relative w-full h-[2.5rem] border-[.5px] border-borders rounded-md pl-2"
+        />
+        <input
+          placeholder="Numero de Documento"
+          type="number"
+          id="document"
+          value={document}
+          onChange={(e) => setDocument(e.target.value)}
+          required
+          className="relative w-full h-[2.5rem] border-[.5px] border-borders rounded-md pl-2"
+        />
+        <input
+          placeholder="Telefono de contacto"
+          type="number"
+          id="phone"
+          value={phone}
+          onChange={(e) => setPhone(e.target.value)}
+          required
+          className="relative w-full h-[2.5rem] border-[.5px] border-borders rounded-md pl-2"
+        />
+        <select
+          name="marital-status"
+          className="border border-borders rounded-md w-full h-[2.5rem] px-2"
+        >
+          <option>Seleccione su obra social</option>
+          {obraSocial.map((name) => (
+            <option value={name.id}>{name.name}</option>
+          ))}
+        </select>
+        <div className="w-full h-[2.5rem]">
           <input
             placeholder="Contraseña"
             type="password"
@@ -77,9 +125,20 @@ const Register = () => {
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
-            className="relative w-full border-[.5px] border-buttonBg rounded-md pl-7"
+            className="relative w-full h-[2.5rem] border-[.5px] border-borders rounded-md pl-2"
           />
-          <Lock />
+          <ButtonEyeSlash />
+        </div>
+        <div className="w-full h-[2.5rem]">
+          <input
+            placeholder="Contraseña"
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+            className="relative w-full h-[2.5rem] border-[.5px] border-borders rounded-md pl-2"
+          />
           <ButtonEyeSlash />
         </div>
         <ButtonLogIn
