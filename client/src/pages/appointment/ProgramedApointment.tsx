1b309168eec5b23bffef314c03db0495f3275e46
mabicruz <cruzmaribel013@gmail.com>
2023-04-05 00:10:03 -0300

    Programmed Appointment styles added

--- client/src/pages/appointment/ProgramedApointment.tsx
+++ client/src/pages/appointment/ProgramedApointment.tsx
@@ -5,8 +5,72 @@ import HeaderSm from "../../components/HeaderSm";
 import NavBar from "../../components/NavBar";
 import Accordion from 'react-bootstrap/Accordion';
 import AllCollapseExample from "../../components/Accordion";
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const ProgramedApointment = () => {
+
+  interface item {
+    estudio: string,
+    especialidad: string,
+    especialistas: string[],
+    fecha:string[],
+    horarios: string[],
+}
+
+  let pedidos : Array<item> =[
+    {
+      estudio: 'Electrocardiograma',
+      especialidad: 'Cardiologia',
+      especialistas: ['Dr. Pedro Pascal' , 'Dr. Strange'],
+      fecha:['26 de Abril', '27 de Abril', '28 de Abril'],
+      horarios: ['09:00', '10:30', '11:00'],
+    },
+    {
+      estudio: 'Colonoscopia',
+      especialidad: 'Gastroenterologia',
+      especialistas: ['Dr. Bruno Diaz' , 'Dra. Martinez'],
+      fecha:['26 de Abril', '27 de Abril', '28 de Abril'],
+      horarios: ['09:00', '10:30', '11:00'],
+    },
+    {
+      estudio: 'Espirometria',
+      especialidad: 'Neumonologia',
+      especialistas: ['Dr. Pedro Pascal' , 'Dr. Strange'],
+      fecha:['26 de Abril', '27 de Abril', '28 de Abril'],
+      horarios: ['09:00', '10:30', '11:00'],
+    }
+  ];
+  
+  interface turno {
+    estudio: string,
+    especialidad: string,
+    especialistas: string[],
+    fecha:string[],
+    horarios: string[],
+}
+
+let reserva: Array<turno> = []
+
+
+interface cita {
+  estudio: string,
+  especialidad: string,
+  especialista: string,
+  fecha:string,
+  horario: string,
+}
+
+let citas: Array<cita> = [
+  {
+    estudio: 'Endoscopia',
+    especialidad: 'Gastroenterologia',
+    especialista: 'Dr. Barty Crouch Jr. ',
+    fecha:'26 de Abril',
+    horario: '09:00',
+  }
+]
+
   return (
     <div className="">
       <HeaderSm />
@@ -18,32 +82,52 @@ const ProgramedApointment = () => {
         third={"Escoge el especialista y el horario:"}
       />
       <div className="flex-col items-center">
-        <AllCollapseExample header="Estudio 1" body="Algo por aca"/>
-        
-        <Accordion className="my-5">
-        <Accordion.Item eventKey="0">
-          <Accordion.Header>Estudio 2</Accordion.Header>
-          <Accordion.Body>
-            <select name="specialist" className="w-full border-2 border-borders my-2">
-              <option >Seleccione su Dr/Dra</option>
-              <option value="">Doc uno</option>
-              <option value="">Doc dos</option>
-              <option value="">Doc tres</option>
-            </select>
-            <select name="date" className="w-full border-2 border-borders my-2">
-              <option >Seleccione el dia</option>
-              <option value="">23 de Abril</option>
-            </select>
-            <select name="schedule" className="w-full border-2 border-borders my-2">
-              <option >Seleccione el horario</option>
-              <option value="">09:00</option>
-              <option value="">09:30</option>
-              <option value="">10:00</option>
-            </select>
-          </Accordion.Body>
-        </Accordion.Item>
+        {
+          
+          citas?.map((cita) => {
+            return(
+              <Accordion defaultActiveKey="0" >
+                <Accordion.Item eventKey="0" >
+                  <Accordion.Header>{cita.estudio}</Accordion.Header>
+                  <Accordion.Body>
+                    <p>Especialista: <span>{cita.especialista}</span></p>
+                    <p>Fecha: <span>{cita.fecha}</span></p>
+                    <p>Horario: <span>{cita.horario}</span></p>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            )
+          }) 
+        }
+          {
 
-      </Accordion>
+            pedidos.map((estudio) => {
+              return(
+                <Accordion className="my-5">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>{estudio.estudio}</Accordion.Header>
+                  <Accordion.Body>
+                    <Form.Select aria-label="Default select example" className="w-full border-2 border-borders my-2">
+                      <option>Seleccione un Especialista</option>
+                      {estudio.especialistas.map((i) => <option value="">{i}</option>)}
+                    </Form.Select>
+                    <Form.Select aria-label="Default select example" className="w-full border-2 border-borders my-2">
+                      <option >Seleccione el dia</option>
+                        {estudio.fecha.map((i) => <option value="">{i}</option>)}
+                    </Form.Select>
+                    <Form.Select aria-label="Default select example" className="w-full border-2 border-borders my-2">
+                      <option >Seleccione el horario</option>
+                        {estudio.horarios.map((i) => <option value="">{i}</option>)}
+                    </Form.Select>
+  
+                    <Button variant="primary" className="w-full">Agendar</Button>
+                  </Accordion.Body>
+                </Accordion.Item>
+        
+              </Accordion>
+              )
+            })
+          }
       </div>
 
     </div>
