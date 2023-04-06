8d9223d68355c810406471ce6e7eb2ea47d4aee4
Matias Barengo <matiasbarengo99@gmail.com>
2023-04-06 18:24:36 -0300

    redux config

---
+++ client/src/interfaces/user.interface.ts
@@ -0,0 +1,9 @@
+export interface User {
+  name: string;
+  secondName: string;
+  document: string;
+  email: string;
+  password: string;
+  phone: string;
+  obraSocialId: string;
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,16 +3,12 @@ import ReactDOM from "react-dom/client";
 import { HashRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import "./index.css";
-import store from "./store";
-import { Provider } from "react-redux";
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
-    </Provider>
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>
 );
