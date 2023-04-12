bfdf42e970a70b7fa1cb3f214f5a47b2f85ccee9
Matias Barengo <matiasbarengo99@gmail.com>
2023-04-12 20:57:05 -0300

    login responsive

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,10 +1,12 @@
 import Logo from "../icons/Logo";
 
-export default function Footer() {
+export default function Footer({ className }) {
   return (
     <div className="flex flex-col items-center  w-full bg-gradient-to-b from-primary-start to-primary-end px-28 py-6 lg:px-10 lg:py-5 lg:h-52 lg:justify-center">
       <Logo />
-      <ul className="flex justify-between w-full text-sm text-white my-2 lg:flex-wrap lg:gap-4 lg:justify-center ">
+      <ul
+        className={`flex justify-between w-full text-sm text-white my-2 lg:flex-wrap lg:gap-4 lg:justify-center ${className}`}
+      >
         <li>
           <a className="font-inter " href="#">
             Sobre Nosotros
--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -7,6 +7,9 @@ import Lock from "../../icons/Lock";
 import MailIcon from "../../icons/MailIcon";
 import { useCustomSelector, useCustomDispatch } from "../../hooks/redux";
 import { login } from "../../redux/slices/auth";
+import Logo from "../../icons/Logo";
+import { AvatarGroup } from "../../icons/AvatarGroup";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -34,45 +37,82 @@ const Login = () => {
   }
 
   return (
-    <div className="flex flex-col justify-center items-center mb-[10rem]">
-      <HeaderSm />
-      <form
-        className="h-[100%] w-[19rem] mt-[3rem] flex flex-col items-center justify-center"
-        onSubmit={submit}
-      >
-        <div className="w-full h-[50px]">
-          <label htmlFor="email" />
-          <input
-            placeholder="Correo Electronico"
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-            className="relative w-full border-[.5px] border-buttonBg rounded-md pl-7"
-          />
-          <MailIcon />
+    <div className="flex">
+      <div className=" flex flex-col justify-between h-[32rem] w-[38rem] bg-gradient-to-b from-primary-start to-primary-end rounded-l-xl p-[2.25rem] lg:hidden">
+        <Logo />
+        <h1 className="text-4xl text-white font-extrabold">
+          La plataforma que te ayuda hacer tus consultas medicas de una forma más rapida.
+        </h1>
+        <p className="text-base text-white">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. A ullam ducimus asperiores modi
+          repellat. Ipsa rem, facilis quia voluptatem enim numquam quos recusandae ipsum! Iste animi
+          fuga reprehenderit id eius.
+        </p>
+        <div className="flex justify-start items-center gap-5">
+          <AvatarGroup />
+          <div className="w-[.5px] h-full bg-[#3F83F8]"></div>
+          <p className="text-secondary">
+            Rated Best Over <strong className="text-white">15.7k</strong> Reviews
+          </p>
         </div>
-        <div className="abolute w-full h-[50px]">
-          <label htmlFor="password" />
-          <input
-            placeholder="Contraseña"
-            type={showPassword ? "password" : "text"}
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-            className="relative w-full border-[.5px] border-buttonBg rounded-md pl-7"
-          />
-          <Lock />
-          <ButtonEyeSlash
-            className="left-[280px] bottom-[43px]"
-            onClick={() => setShowPassword(!showPassword)}
-          />
-        </div>
-        <ButtonLogIn className="mt-[3rem]">Iniciar Sesion</ButtonLogIn>
-      </form>
-      {isLoading && "cargando..."}
+      </div>
+      <div className="w-[23rem] h-[32rem] bg-white flex flex-col justify-center items-center mb-[5rem] rounded-r-xl lg:rounded-xl">
+        <HeaderSm />
+        <h2 className="text-xl">Ingresa a Citas Medicas a un Click</h2>
+        <form
+          className="h-fit w-[19rem] mt-[3rem] flex flex-col items-start justify-center gap"
+          onSubmit={submit}
+        >
+          <div className="flex flex-col justify-start gap-10 w-full">
+            <div className="w-full h-[50px] ">
+              <p className="mb-2">Tu correo electronico</p>
+              <label htmlFor="email" />
+              <input
+                placeholder="Correo Electronico"
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+                className="relative w-full border-[.5px] h-[2.5rem] border-buttonBg rounded-md pl-7"
+              />
+              <MailIcon />
+            </div>
+            <div className="w-full">
+              <p className="mb-2">Contraseña</p>
+              <label htmlFor="password" />
+              <input
+                placeholder="Contraseña"
+                type={showPassword ? "password" : "text"}
+                id="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                className="relative w-full h-[2.5rem] border-[.5px] border-buttonBg rounded-md pl-7"
+              />
+              <Lock />
+              <ButtonEyeSlash
+                className="left-[280px] bottom-[43px]"
+                onClick={() => setShowPassword(!showPassword)}
+              />
+            </div>
+            <div className="flex justify-end">
+              <p className="text-primary-start text-xs pb-6">¿Olvidaste tu contraseña?</p>
+            </div>
+          </div>
+          <ButtonLogIn className="bg-primary-start">INGRESAR</ButtonLogIn>
+          <div>
+            <p className="text-xs pt-4">
+              ¿No tienes un cuenta?{" "}
+              <Link to={"/register"} className="text-primary-start">
+                Crea una cuenta
+              </Link>
+              .
+            </p>
+          </div>
+        </form>
+        {isLoading && "cargando..."}
+      </div>
     </div>
   );
 };
