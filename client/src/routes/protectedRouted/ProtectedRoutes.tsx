c5a116e87eb2a886e36192dea6d2511e9b59e532
David <david.cicconi94@gmail.com>
2023-04-18 13:29:32 -0300

    archivos borrados

--- client/src/routes/protectedRouted/ProtectedRoutes.tsx
+++
@@ -1,6 +0,0 @@
-import { Component } from "react";
-import { Outlet } from "react-router-dom";
-
-export const ProtectedRoute = (children:any) => {
-  return children ? children : <Outlet />;
-};  // end ProtectedRoute
