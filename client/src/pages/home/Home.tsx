0425c99386a7c593d44773906c82090a42f66d05
Matias Barengo <matiasbarengo99@gmail.com>
2023-04-22 07:06:31 -0300

    final commit

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -62,58 +62,6 @@ const Home = () => {
     });
   };
 
-  const infoDoctors = [
-    {
-      id: 1,
-      name: "Daniel Perez",
-      specialty: "Cardiologo",
-      rating: 45,
-      img: "https://randomuser.me/api/portraits/med/men/57.jpg",
-    },
-    {
-      id: 2,
-      name: "Miguel Sanchez",
-      specialty: "Neurologo",
-      rating: 62,
-      img: "https://randomuser.me/api/portraits/med/men/86.jpg",
-    },
-    {
-      id: 3,
-      name: "Pedro Lopez",
-      specialty: "Oftalmologo",
-      rating: 54,
-      img: "https://hips.hearstapps.com/hmg-prod/images/portrait-of-a-happy-young-doctor-in-his-clinic-royalty-free-image-1661432441.jpg?crop=0.66698xw:1xh;center,top&resize=1200:*",
-    },
-    {
-      id: 4,
-      name: "Juan Marcuay",
-      specialty: "Otorrino",
-      rating: 80,
-      img: "https://randomuser.me/api/portraits/med/men/99.jpg",
-    },
-    {
-      id: 5,
-      name: "Fernando Loayza",
-      specialty: "Cardiologo",
-      rating: 90,
-      img: "https://randomuser.me/api/portraits/med/men/82.jpg",
-    },
-    {
-      id: 6,
-      name: "Fernaddo Loayza",
-      specialty: "Cardiologo",
-      rating: 90,
-      img: "https://randomuser.me/api/portraits/med/men/85.jpg",
-    },
-    {
-      id: 7,
-      name: "Fernanssdo Loayza",
-      specialty: "Cardiologo",
-      rating: 90,
-      img: "https://randomuser.me/api/portraits/med/men/77.jpg",
-    },
-  ];
-
   return (
     <div className="flex flex-col justify-center">
       <Header />
